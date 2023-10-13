8b03ee01da63c8abf9939e859320481bf4ccf766
whipbaek <jibaek0513@gmail.com>
2023-10-13 19:59:17 +0900

    chap 3

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
+  constructor(private boardService: BoardsService) {}
 
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+
+  @Get('/hello')
+  getTestBoard(@Query('id') id: string): Board[] {
+    console.log(id);
+    return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardService(
+    @Param(':id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardService.updateBoardStatus(id, status);
+  }
 }
